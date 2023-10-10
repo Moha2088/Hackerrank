05cbe5c3e1b8c3254c0d0b6c0ea138f1c92a92fd
Mohamed Shil <maxamed-14@outlook.dk>
2023-10-10 12:12:07 +0200

    Added SalesByMatch Solution

---
+++ Sales_By_Match/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Solution
+{
+    static void Main(string[] args)
+    {
+        static int sockMerchant(int n, List<int> ar)
+        {
+            foreach (int sock in ar)
+            {
+                List<int> DistinctSocks = ar.Distinct().ToList();
+
+                foreach (int distinctSock in DistinctSocks)
+                {
+                    if (sock == distinctSock)
+                    {
+                        ar.Remove(sock);
+                    }
+                }
+            
+            // var printPairs = from item in ar;
+            }
+
+            return 0;
+        }
+    }
+}
