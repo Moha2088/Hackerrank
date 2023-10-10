87326e6e1ebdf14cee146e3d0955e87389184a35
Mohamed Shil <maxamed-14@outlook.dk>
2023-10-10 10:09:39 +0200

    Readded staircase

---
+++ Stair_Case/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace StairCase
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            static void staircase(int n)
+            {
+                // 1. Udfyld et array med (n - 1) antal spaces, slut med en * og print indholdet ud
+                
+                List<string> Spaces = new List<string>();
+
+                for (int j = 0; j < n - 1; j++)
+                {
+                    Spaces.Add(" ");
+                }
+
+                Spaces.Add("*");
+
+                var firstStep = string.Join("", Spaces);
+                Console.WriteLine(firstStep);
+
+                // 2. Fjern et space og erstat det med en * og print den ud, og fortsæt indtil: n = 0
+                
+                for (int j = n; j > 1; j--)
+                {
+                    Spaces.Remove(" ");
+                    Spaces.Add("*");
+
+                    var remainingSteps = string.Join("", Spaces);
+                    Console.WriteLine(remainingSteps);
+                }
+            }
+
+            staircase(3);
+        }
+    }
+}
